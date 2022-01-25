fd9cd0606a123444f3392c12d84ba22c5ab5ec48
sarahwylie <sarahannemariawylie@gmail.com>
2022-01-24 19:52:49 -0600

    added the party_name into the candidate table

--- server.js
+++ server.js
@@ -41,7 +41,11 @@ const db = mysql.createConnection(
 
 //search all candidates
 app.get('/api/candidates', (req, res) => {
-   const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
     if (err) {
@@ -56,7 +60,12 @@ app.get('/api/candidates', (req, res) => {
 
 //search candidates by id
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id=?`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, rows) => {
