b4d874a7eafda0a289dfdac5f59b77e6eedaea43
Sarah Wylie <90208612+sarahwylie@users.noreply.github.com>
2022-01-25 18:35:18 -0600

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'J@maicanmecrazy',
+        password: '',
         database: 'election'
     },
     console.log('Connected to the election database.')
